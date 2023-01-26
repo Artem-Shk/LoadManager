bbc81b0dd8acbcce7456f54dd0c0230bcbaa2c8d
Artem-Shk <manalitovich@gmail.com>
2023-01-26 17:42:07 +0300

    Добавьте файлы проекта.

---
+++ Model/DataModels/MainTb.cs
@@ -0,0 +1,35 @@
+﻿namespace LoadManager.Model.DataModels
+{
+    public class MainTb
+    {
+        public int Id { get; set; }
+        public string FileName
+        {
+            get
+            {
+                if (FileName != null)
+                {
+                    return FileName;
+
+                }
+                else
+                {
+                    return "FileName is null";
+                }
+            }
+            set { }
+        }
+        public FileData FileData
+        {
+            get { return FileData; }
+            set { if (value is FileData) { FileData = value; }
+
+                // TODO: learning about exceptions and add it here
+                else {
+                    
+                }
+            }
+
+        }
+    }
+}
---
+++ Model/MainContext.cs
@@ -0,0 +1,10 @@
+﻿using LoadManager.Model.DataModels;
+
+namespace LoadManager.Model
+{
+    public class MainContext
+    {
+        List<FileData> FiledataContext = new List<FileData>();
+        List<MainTb> mainTbContext= new List<MainTb>();
+    }
+}
